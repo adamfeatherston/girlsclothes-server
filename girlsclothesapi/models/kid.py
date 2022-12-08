a00ae59324143cf03db38606ff2ca844114a3656
Adam Featherston <j.adam.featherston@gmail.com>
2022-12-08 14:12:45 -0600

    crud for clothing_items

--- girlsclothesapi/models/kid.py
+++ girlsclothesapi/models/kid.py
@@ -12,3 +12,7 @@ class Kid(models.Model):
     pant_size = models.CharField(max_length=5, null=True, blank=True)
     underwear_or_diaper_size = models.CharField(max_length=5, null=True, blank=True)
     sock_size = models.CharField(max_length=5, null=True, blank=True)
+
+    @property
+    def kid_nickname(self):
+        return f'{self.user.username} {self.user.first_name}'
